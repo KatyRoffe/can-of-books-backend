9e48a00bd05660bb557021fbf979f7438526996c
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-27 14:52:09 -0700

    Brought in Mongoose.

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/can-of-books');
+const Book = mongoose.model('book', {name: String});
+const novel = new Book({name: 'novel'});
+novel.save().then(() => console.log('I am a book.'))
+
 app.get('/test', (request, response) => {
 
   response.send('test request received')
