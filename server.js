16277854b51b6d35eab70f37c22db113325d0dc3
Ediberto Ponce <ediberto01@gmail.com>
2021-09-28 15:01:22 -0700

    troubleshooting mongo

--- server.js
+++ server.js
@@ -17,47 +17,49 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/can-of-books');
 
 // this is hanging out in this file for now until we can get the seeding issue figured out.
+
 // somehow we wound up with 12 book instances in our database due to experimentation and we need to figure out how to remove them
-async function seed() {
+const BookModel = require('./models/schema.js');
+// async function seed() {
 
-  mongoose.connect(process.env.DATABASE_URL);
-  const BookModel = require('./models/schema.js');
+//   mongoose.connect(process.env.DATABASE_URL);
+  
 
-    try {
-    await BookModel.create({
-      title: 'Jane Eyre',
-      description: 'Orphan girl becomes nanny',
-      status: 'Read',
-      email: 'brynthepigeon@gmail.com',  
-    })
+//     try {
+//     await BookModel.create({
+//       title: 'Jane Eyre',
+//       description: 'Orphan girl becomes nanny',
+//       status: 'Read',
+//       email: 'brynthepigeon@gmail.com',  
+//     })
       
-    await BookModel.create({
-        title: 'Divergent',
-        description: 'Angsty teens try to overthrow government',
-        status: 'Read',
-        email: 'fallingsnowglobes@gmail.com',  
-      })
+//     await BookModel.create({
+//         title: 'Divergent',
+//         description: 'Angsty teens try to overthrow government',
+//         status: 'Read',
+//         email: 'fallingsnowglobes@gmail.com',  
+//       })
     
-      await BookModel.create({
-        title: 'Little Book of Calm',
-        description: 'Mantras for daily calm',
-        status: 'Unread',
-        email: 'blahblahemailhere@gmail.com',  
-      })
-      await BookModel.create({
-        title: 'foo',
-        description: 'foo',
-        status: 'foo',
-        email: 'foomail@gmail.com',  
-      })
-    } catch (error) {
-        console.error(error);
-    }
+//       await BookModel.create({
+//         title: 'Little Book of Calm',
+//         description: 'Mantras for daily calm',
+//         status: 'Unread',
+//         email: 'blahblahemailhere@gmail.com',  
+//       })
+//       await BookModel.create({
+//         title: 'foo',
+//         description: 'foo',
+//         status: 'foo',
+//         email: 'foomail@gmail.com',  
+//       })
+//     } catch (error) {
+//         console.error(error);
+//     }
 
-    mongoose.disconnect();
-}
+//     mongoose.disconnect();
+// }
 
-seed();
+// seed();
 
 
 //routes
@@ -67,14 +69,14 @@ app.get('/test', (request, response) => {
 })
 
 app.get('/books', async (req, res) => {
-  // const filterQuery = {};
-  console.log('books');
-  // if (req.query.title) {
-  //   filterQuery.title = req.query.title;
-  // }
-  // const books = await BookModel.find(filterQuery);
-
-  res.send('test pending ');
+  const filterQuery = {};
+  // console.log('books');
+  if (req.query.email) {
+    filterQuery.email = req.query.email;
+  }
+  const books = await BookModel.find(filterQuery);
+  // console.log(books)
+  res.send(books);
 })
 
 
