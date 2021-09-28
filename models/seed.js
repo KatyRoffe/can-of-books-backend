27db06c027fa410e1d3bb6e61066b524a35ff8ea
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-27 18:44:52 -0700

    Got book instances created in database.

--- models/seed.js
+++ models/seed.js
@@ -1,36 +1,45 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// objects would not seed by using this file. per JB, they are residing in the server.js for now until we can figure it out.
 
-mongoose.connect(process.env.DATABASE_URL);
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-const BookModel = require('./schema.js');
+// async function seed() {
 
-async function seed() {
-    try {
-      await BookModel.create({
-      title: 'Jane Eyre',
-      description: 'Orphan girl becomes nanny',
-      status: 'Read',
-      email: 'brynthepigeon@gmail.com',  
-    })
+//   mongoose.connect('mongodb://localhost:27017/can-of-books');
+//   const BookModel = require('./schema.js');
+
+//     try {
+//       await BookModel.create({
+//       title: 'Mary Stuff',
+//       description: 'Orphan girl becomes nanny',
+//       status: 'Read',
+//       email: 'brynthepigeon@gmail.com',  
+//     })
       
-    // await BookModel.create({
-    //     title: 'Divergent',
-    //     description: 'Angsty teens try to overthrow government',
-    //     status: 'Read',
-    //     email: 'fallingsnowglobes@gmail.com',  
-    //   })
+//     await BookModel.create({
+//         title: 'Divergent',
+//         description: 'Angsty teens try to overthrow government',
+//         status: 'Read',
+//         email: 'fallingsnowglobes@gmail.com',  
+//       })
     
-    //   await BookModel.create({
-    //     title: 'Little Book of Calm',
-    //     description: 'Mantras for daily calm',
-    //     status: 'Unread',
-    //     email: 'blahblahemailhere@gmail.com',  
-    //   })
-    } catch (error) {
-        console.error(error);
-    }
-    mongoose.disconnect();
-}
+//       await BookModel.create({
+//         title: 'Little Book of Calm',
+//         description: 'Mantras for daily calm',
+//         status: 'Unread',
+//         email: 'blahblahemailhere@gmail.com',  
+//       })
 
-seed();
+//       await BookModel.create({
+//         title: 'foo',
+//         description: 'foo',
+//         status: 'foo',
+//         email: 'foomail@gmail.com',  
+//       })
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     mongoose.disconnect();
+// }
+
+// seed();
--- server.js
+++ server.js
@@ -13,10 +13,51 @@ const PORT = process.env.PORT || 3001;
 // initialize mongoose
 const mongoose = require('mongoose');
 
-const BookModel = require('./models/schema.js')
+// const BookModel = require('./models/schema.js')
+// mongoose.connect('mongodb://localhost:27017/can-of-books');
 
+// this is hanging out in this file for now until we can get the seeding issue figured out.
+// somehow we wound up with 12 book instances in our database due to experimentation and we need to figure out how to remove them
+async function seed() {
 
-mongoose.connect('mongodb://localhost:27017/can-of-books');
+  mongoose.connect(process.env.DATABASE_URL);
+  const BookModel = require('./models/schema.js');
+
+    try {
+    await BookModel.create({
+      title: 'Jane Eyre',
+      description: 'Orphan girl becomes nanny',
+      status: 'Read',
+      email: 'brynthepigeon@gmail.com',  
+    })
+      
+    await BookModel.create({
+        title: 'Divergent',
+        description: 'Angsty teens try to overthrow government',
+        status: 'Read',
+        email: 'fallingsnowglobes@gmail.com',  
+      })
+    
+      await BookModel.create({
+        title: 'Little Book of Calm',
+        description: 'Mantras for daily calm',
+        status: 'Unread',
+        email: 'blahblahemailhere@gmail.com',  
+      })
+      await BookModel.create({
+        title: 'foo',
+        description: 'foo',
+        status: 'foo',
+        email: 'foomail@gmail.com',  
+      })
+    } catch (error) {
+        console.error(error);
+    }
+
+    mongoose.disconnect();
+}
+
+seed();
 
 
 //routes
@@ -34,6 +75,7 @@ app.get('/books', async (req, res) => {
 
   res.send(books);
 })
+
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
